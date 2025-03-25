f6f25cdfee6e70f5be0c33a4e73eadd744cb8765
vws-codes <vsunder2@uwo.ca>
2025-03-24 21:30:46 -0400

    update API URL and server name in configuration files

--- web-pod/src/App.js
+++ web-pod/src/App.js
@@ -33,7 +33,7 @@ const App = () => {
 	const [nickname, setNickname] = useState("");
 	const [isEditingNickname, setIsEditingNickname] = useState(false);
 
-	const API_URL = "https://socialm.duckdns.org";
+	const API_URL = "https://socialmapp.duckdns.org";
 
 	// Handle Firebase auth state
 	useEffect(() => {
